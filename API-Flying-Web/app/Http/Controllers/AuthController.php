ebc29299cfe10342d1e058fa28ff7a564b5d20e6
Evan-Duval <duvalevan368@gmail.com>
2025-01-07 12:23:33 +0100

    register begining

--- API-Flying-Web/app/Http/Controllers/AuthController.php
+++ API-Flying-Web/app/Http/Controllers/AuthController.php
@@ -16,25 +16,31 @@ class AuthController extends Controller
     /**
     * Create user
     *
-    * @param  [string] name
+    * @param  [string] first_name
+    * @param  [string] last_name
     * @param  [string] email
     * @param  [string] password
     * @param  [string] password_confirmation
+    * @param  [date] birthday
     * @return [string] message
     */
     public function register(Request $request)
     {
         $request->validate([
-            'name' => 'required|string',
+            'first_name' => 'required|string',
+            'last_name' => 'required|string',
             'email'=>'required|string|unique:users',
             'password'=>'required|string|min:8',
-            'c_password' => 'required|same:password'
+            'c_password' => 'required|same:password',
+            'birthday' => 'required|date',
         ]);
 
         $user = new User([
-            'name'  => $request->name,
+            'first_name'  => $request->first_name,
+            'last_name'  => $request->last_name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
+            'birthday' => $request->birthday,
         ]);
 
         if($user->save()){
