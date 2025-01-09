e59b871792276328611026fb881c7244a0f696b8
Evan-Duval <duvalevan368@gmail.com>
2025-01-09 10:02:15 +0100

    fix: style and button problems

--- FrontEnd/admin/admin.php
+++ FrontEnd/admin/admin.php
@@ -3,6 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet">
+    <!-- Flatpickr CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
     <link rel="stylesheet" href="admin.css">
     <title>Admin</title>
 </head>
@@ -11,18 +14,20 @@
     <?php 
         include '../components/navbar.php';
     ?>
-
-    <button>
-        <a href="plane/plane.php">Ajouter un avion</a>
-    </button> 
+  
+    <a href="plane/plane.php">
+        <button>Ajouter un avion</button> 
+    </a>
     
-    <button>
-        <a href="aeroport/aeroport.php">Ajouter un aéroport</a>
-    </button>    
+    <a href="aeroport/aeroport.php">
+        <button>Ajouter un aéroport</button>
+    </a>
+    
+    <a href="piste/piste.php">
+        <button>Ajouter une/des piste(s) à un aéroport</button>
+    </a>
 
-    <button>
-        <a href="piste/piste.php">Ajouter une/des piste(s) à un aéroport</a>
-    </button>    
-
+    <!-- Flatpickr JS -->
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 </body>
 </html>
