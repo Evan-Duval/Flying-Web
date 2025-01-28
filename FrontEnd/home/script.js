c0b5913d4d843bc4026d95e271845b17f45b727f
Evan-Duval <duvalevan368@gmail.com>
2025-01-28 18:32:47 +0100

    add: main code

--- FrontEnd/home/script.js
+++ FrontEnd/home/script.js
@@ -2,17 +2,6 @@ const typeSelectionne = document.getElementById('type');
 const dateRetour = document.getElementById('date-retour');
 const dateRetourLabel = document.getElementById('date-retour-label');
 
-typeSelectionne.addEventListener('change', () => {
-    if (typeSelectionne.value === 'aller-retour') {
-        dateRetour.style.display = 'block'; //vu
-        dateRetourLabel.style.display = 'block'; //vu
-    } else {
-        dateRetour.style.display = 'none'; //cacher
-        dateRetourLabel.style.display = 'none'; //cacher
-    }
-});
-
-
 // Calendrier Personnailé, bibliotheque js (flatpickr)
 flatpickr("#date", {
     dateFormat: "Y-m-d",  
